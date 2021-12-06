d6a592a780e970ffcc2b509ec3bf9ce646f4da77
Piotr Kurylak <piotrkurylak@gmail.com>
2021-12-06 11:05:32 +0100

    Added function for transfering files to remote machine

---
+++ backup_files.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Create compressed archive.
+
+set -eu
+
+lock_file_name="${0}.lock"
+lock_file_path="/var/lock/"
+lock_file="${lock_file_path}${lock_file_name}"
+backup_dir="${1}"
+backup_file_name="backup.tar.gz"
+backup_full_name="$(date +%F-%H-%M)"."${backup_file_name}"
+identity_file="${2}"
+username="${3}"
+remote_ip="${4}"
+remote_dest=${5}
+
+function is_running() {
+    if [[ -e "${lock_file}" ]]; then
+        printf "Found lock file, another instance of script is running?.\nExiting.\n" >&2
+        exit 1
+    fi
+}
+
+function create_lock_file() {
+    touch "${lock_file}" || {
+        printf "Cannot create lock file.\nExiting.\n" >&2
+        exit 2
+    }
+}
+
+function delete_lock_file() {
+    rm -f "${lock_file}" || {
+        printf "Cannot delete lock file.\nExiting.\n" >&2
+        exit 3
+    }
+}
+
+
+function handle_overwrite() {
+    if [[ -e "${backup_full_name}" ]]; then
+        printf "Found copy of backup file.\nExiting.\n" >&2
+        delete_lock_file
+        exit 5
+    fi
+}
+
+function archive_files() {
+    tar -zpcf "${backup_full_name}" "${backup_dir}" || {
+        printf "Cannot archive files.\nExiting.\n" >&2
+        delete_lock_file
+        exit 6
+    }
+}
+
+function copy_files() {
+    scp -i "${identity_file}" "${backup_full_name}" "${username}"@"${remote_ip}":"${remote_dest}" || {
+        printf "Cannot transfer files to remote server.\nExiting\n" >&2
+        delete_lock_file
+        exit 7
+    }
+}
+
+
+trap delete_lock_file SIGINT SIGTERM
+
+is_running
+create_lock_file
+handle_overwrite
+archive_files
+## Only for lock tests.
+sleep 5
+copy_files
+delete_lock_file
+
+## TODO
+## flock instead of touch lock file
+## local variables?
+## add connection to remote server
+
